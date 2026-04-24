5cdc111c9f60888b5c64780673fc806c03664b91
armigodf <davydov.danil.2007@mail.ru>
2026-04-24 20:11:43 +0300

    первая версия создания архива

--- laba-1.py
+++ laba-1.py
@@ -1,3 +1,11 @@
-print(7 + 8)
+import os
 
-print(7 + 8)
+
+def sozdat_archiv(archiv_name, folder_line):
+    with open(archiv_name, "wb") as archiv:  # Открывает или записывает файл в бинарном виде
+        for i in os.listdir(folder_line):
+            file_line = os.path.join(folder_line, i)
+            if os.path.isfile(file_line):
+                name_v_bytax = i.encode('utf_8')
+                with open(file_line, "rb") as f:
+                    soderzhimoe = f.read()
