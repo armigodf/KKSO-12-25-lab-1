a3380e0bc3a0fef3a7b44530c359c59501cd3129
armigodf <davydov.danil.2007@mail.ru>
2026-04-24 21:30:47 +0300

    создал функцию распаковки и добавил новую библиотеку

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import struct
 
 
 def sozdat_archiv(archiv_name, folder_line):
@@ -9,3 +10,22 @@ def sozdat_archiv(archiv_name, folder_line):
                 name_v_bytax = i.encode('utf_8')
                 with open(file_line, "rb") as f:
                     soderzhimoe = f.read()
+
+
+def raspakoyka_archive(arcgive_name, folder_mesto):
+    if not os.path.exists((folder_mesto)):
+        os.makedirs(folder_mesto)
+
+    with open(archive_name, "rb") as archive:
+        while True:
+            data = archive.read(4)
+            if not data:
+                break
+            len_name = struct.raspakoyka_archive("I", data)[0]
+            filename = archive.read(len_name).decode('utf-8')
+            len_soderzhimoe = struct.raspakoyka_archive("Q", archive.read(8))[0]
+            soderzhimoe = archive.read((len_soderzhimoe))
+
+            with open(os.path.join(folder_mesto, filename), "wb") as f:
+                f.write(soderzhimoe)
+    print(f"Архив распакован в {folder_mesto}")
